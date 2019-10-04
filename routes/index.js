2b5781f80444232ba12f4bd9c376ed13921d1b69
Aleksa Djokic <djokicaleksa@gmail.com>
2019-10-04 23:27:54 +0200

    settings (crud) controller

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var router = express.Router()
 
-const { read, update } = require('../controllers/SettingsController')
+const { create, read, update, destroy } = require('../controllers/settingsController')
 
 // define the home page route
 router.get('/settings', read)
 router.put('/settings/:id', update)
+router.delete('/settings/:id', destroy)
+router.post('/settings', create)
+
+
 module.exports = router
