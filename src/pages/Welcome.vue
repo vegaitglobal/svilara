8bc02ced7823d5cbf076dc1adc479186dba97f4c
Ana Bjelica <a.bjelica@protokite.com>
2019-10-04 22:41:09 +0200

    Add new color variables, update reset file

--- src/pages/Welcome.vue
+++ src/pages/Welcome.vue
@@ -1,19 +1,15 @@
 <template>
   <div>
-    <HelloWorld msg="Test Message"></HelloWorld>
-    <Sidebar/>
+
   </div>
 </template>
 
 <script>
-import { Sidebar } from "@/components";
+
 
 
 export default {
-  components: {
 
-    Sidebar
-  }
 };
 
 </script>
@@ -21,13 +17,5 @@ export default {
 <style scoped lang="scss">
 @import "../assets/scss/variables.scss";
 
-div {
-	width: 100%;
-	max-width: 250px;
-	background-color: $red;
-	display: inline-block;
-	margin: 0 15px 15px 0;
-	border: 1px solid #D1D2D4;
-	vertical-align: top;
-}
+
 </style>
