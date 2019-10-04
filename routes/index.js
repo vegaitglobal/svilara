de1db50a22c0f8d53da0b304765791db6a1f4e16
Gavrilo Prodanovic <mail.gavrilo@gmail.com>
2019-10-04 23:56:18 +0200

    Questions controller implemented. Create method missing

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,19 @@ var express = require('express')
 var router = express.Router()
 
 const { create, read, update, destroy } = require('../controllers/settingsController')
+const { create_question, read_question, update_question, 
+    delete_question } = require('../controllers/questionsController')
 
 // define the home page route
 router.get('/settings', read)
 router.put('/settings/:id', update)
 router.delete('/settings/:id', destroy)
 router.post('/settings', create)
+
+router.get('/questions', read_question)
+router.put('/questions/:id', update_question)
+router.delete('/questions/:id', delete_question)
+router.post('/questions', create_question)
 
 
 module.exports = router
