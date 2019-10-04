eacdd64a58246cbcac5d2b58250627baae45fcf1
Dunja Vasic <dunjavasic.pzs@gmail.com>
2019-10-04 22:03:41 +0200

    added sidebar

--- src/pages/Welcome.vue
+++ src/pages/Welcome.vue
@@ -1,13 +1,19 @@
 <template>
-  <HelloWorld msg="Test Message"></HelloWorld>
+  <div>
+    <HelloWorld msg="Test Message"></HelloWorld>
+    <Sidebar/>
+  </div>
 </template>
 
 <script>
 import { HelloWorld } from "@/components";
+import { Sidebar } from "@/components";
 
 export default {
   components: {
     HelloWorld,
+    Sidebar
   }
 };
+
 </script>
