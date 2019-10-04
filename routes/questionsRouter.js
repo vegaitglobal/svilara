74178e14341d8c118385d6001ea035a3c8722c72
Aleksa Djokic <djokicaleksa@gmail.com>
2019-10-05 00:23:20 +0200

    questions and settings routes prefix fixed

--- routes/questionsRouter.js
+++ routes/questionsRouter.js
@@ -4,9 +4,9 @@ var router = express.Router()
 const { create_question, read_question, update_question, 
     delete_question } = require('../controllers/questionsController')
 
-router.get('/questions', read_question)
-router.put('/questions/:id', update_question)
-router.delete('/questions/:id', delete_question)
-router.post('/questions', create_question)
+router.get('/', read_question)
+router.put('/:id', update_question)
+router.delete('/:id', delete_question)
+router.post('/', create_question)
 
 module.exports = router;
--- routes/settingsRouter.js
+++ routes/settingsRouter.js
@@ -4,9 +4,9 @@ var router = express.Router()
 const { createSettings, readSettings, updateSettings, destroySettings } = require('../controllers/settingsController')
 
 // define the home page route
-router.get('/settings', readSettings)
-router.put('/settings/:id', updateSettings)
-router.delete('/settings/:id', destroySettings)
-router.post('/settings', createSettings)
+router.get('/', readSettings)
+router.put('/:id', updateSettings)
+router.delete('/:id', destroySettings)
+router.post('/', createSettings)
 
 module.exports = router
