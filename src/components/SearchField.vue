35273f1f1abb0161fc0eb44623f6188a7f13d511
Ana Bjelica <a.bjelica@protokite.com>
2019-10-05 11:27:41 +0200

    Add search field component

---
+++ src/components/SearchField.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="search-field">
+    <magnify-icon class="search-field--ico"/>
+    <input class="search-field--input" type="text" placeholder="Pretraži..." />
+  </div>
+</template>
+
+<script>
+import MagnifyIcon from 'vue-material-design-icons/Magnify.vue';
+
+export default {
+    name: "SearchField",
+    components: {
+        MagnifyIcon
+    }
+}
+</script>
+
+<style lang="scss">
+@import "../assets/scss/variables.scss";
+
+.search-field {
+    margin-right: 30px;
+    .search-field--ico {
+        font-size: 20px;
+        svg {
+            fill: $purple;
+        }
+    }
+    &--input {
+        font-size: 16px;
+        line-height: 20px;
+        background-color: transparent;
+        border: 0;
+        color: rgba($purple, 0.5);
+        padding: 5px;
+
+        &::placeholder {
+            font-size: 16px;
+            line-height: 20px;
+            color: rgba($purple, 0.5);
+        }
+    }
+}
+
+</style>
