8b278c4049d65a6b5d9f4bf404e8db77542b0c86
Miloš <zeka035@gmail.com>
2019-10-05 20:10:56 +0200

    fuse

--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -1,93 +1,119 @@
-import axios from 'axios';
-import * as _ from 'lodash';
+import axios from "axios";
+import * as _ from "lodash";
+import Fuse from "fuse.js";
+var fuseOptions = {
+  shouldSort: true,
+  threshold: 0.6,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  keys: ["title"]
+};
 
 export default {
-    state: {
-        questions: [],
-        answers: [],
-        image: '',
+  state: {
+    questions: [],
+    answers: [],
+    image: "",
 
-        events: []
+    events: []
+  },
+
+  getters: {
+    getEvents(state) {
+      return state.events;
     },
-
-    getters: {
-        getEvents(state) {
-            return state.events;
-        }
-    },
-
-    mutations: {
-        SET_QUESTIONS(state, questions) {
-            state.questions = questions
-        },
-
-        ANSWER_QUESTION(state, questionAnswers) {
-            state.answers.push(questionAnswers)
-        },
-
-        REMOVE_ANSWER(state, id) {
-            for (var i = 0; i < state.answers.length; i++) {
-                if (state.answers[i].question.id === id) {
-                    state.answers.splice(i, 1)
-                }
-            }
-        },
-        SET_IMAGE(state, file) {
-            state.image = file
-        },
-
-        SET_EVENTS(state, events){
-            state.events = events;
-        }
-    },
-
-    actions: {
-        async fetchQuestions({ commit }) {
-            return new Promise((resolve, reject) => {
-                axios.get(`${process.env.VUE_APP_BASE_URL}/admin/questions`).then(res => {
-                    commit('SET_QUESTIONS', res.data.data)
-                    resolve(res.data.data)
-                }).catch(err => {
-                    reject(err)
-                })
-
-            })
-        },
-
-        async answerQuestion({ commit }, { question, answers }) {
-            commit('REMOVE_ANSWER', question.id)
-            commit('ANSWER_QUESTION', { question: { id: question.id, text: question.text }, answers, type: question.fieldType, name: question.name })
-        },
-
-        async setImage({ commit }, file) {
-            commit('SET_IMAGE', file)
-        },
-
-        async submitEvent({ commit, state }) {
-            var formData = new FormData();
-            for (var i = 0; i < state.answers.length; i++){
-
-                if(state.answers[i].type === 'file'){
-                    formData.append(state.answers[i].name, state.answers[i].answers);     
-                }
-            }
-            formData.set('formAnswers', JSON.stringify(state.answers));
-          
-            axios.post(`${process.env.VUE_APP_BASE_URL}/user/event`, formData).then(res => {
-                console.log(res)
-            }).catch(err => {
-                console.log(err)
-            })
-        },
-
-        async fetchEvents({commit}) {
-            try{
-                const events = await axios.get(`${process.env.VUE_APP_BASE_URL}/user/events`);
-                commit('SET_EVENTS', events.data.data)
-                return events;
-            }catch(err){
-                return err
-            }
-        }
+    searchEvent(state, query) {
+      let fuse = new Fuse(state.events, options);
+      let result = fuse.search(query);
+      return result;
     }
-}
+  },
+
+  mutations: {
+    SET_QUESTIONS(state, questions) {
+      state.questions = questions;
+    },
+
+    ANSWER_QUESTION(state, questionAnswers) {
+      state.answers.push(questionAnswers);
+    },
+
+    REMOVE_ANSWER(state, id) {
+      for (var i = 0; i < state.answers.length; i++) {
+        if (state.answers[i].question.id === id) {
+          state.answers.splice(i, 1);
+        }
+      }
+    },
+    SET_IMAGE(state, file) {
+      state.image = file;
+    },
+
+    SET_EVENTS(state, events) {
+      state.events = events;
+    }
+  },
+
+  actions: {
+    async fetchQuestions({ commit }) {
+      return new Promise((resolve, reject) => {
+        axios
+          .get(`${process.env.VUE_APP_BASE_URL}/admin/questions`)
+          .then(res => {
+            commit("SET_QUESTIONS", res.data.data);
+            resolve(res.data.data);
+          })
+          .catch(err => {
+            reject(err);
+          });
+      });
+    },
+
+    async answerQuestion({ commit }, { question, answers }) {
+      commit("REMOVE_ANSWER", question.id);
+      commit("ANSWER_QUESTION", {
+        question: { id: question.id, text: question.text },
+        answers,
+        type: question.fieldType,
+        name: question.name
+      });
+    },
+
+    async setImage({ commit }, file) {
+      commit("SET_IMAGE", file);
+    },
+
+    async submitEvent({ commit, state }) {
+      var formData = new FormData();
+      for (var i = 0; i < state.answers.length; i++) {
+        if (state.answers[i].type === "file") {
+          formData.append(state.answers[i].name, state.answers[i].answers);
+        }
+      }
+      formData.set("formAnswers", JSON.stringify(state.answers));
+
+      axios
+        .post(`${process.env.VUE_APP_BASE_URL}/user/event`, formData)
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+
+    async fetchEvents({ commit }) {
+      try {
+        const events = await axios.get(
+          `${process.env.VUE_APP_BASE_URL}/user/events`
+        );
+        commit("SET_EVENTS", events.data.data);
+        return events;
+      } catch (err) {
+        return err;
+      }
+    }
+  }
+};
