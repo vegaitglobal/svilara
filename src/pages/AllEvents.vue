1f73aca2e9a76b8544dd3e0a8d7111c9eaa50c45
Ana Bjelica <a.bjelica@protokite.com>
2019-10-06 14:51:41 +0200

    Style the table for all events

--- src/pages/AllEvents.vue
+++ src/pages/AllEvents.vue
@@ -28,4 +28,47 @@ export default {
 </script>
 
 <style lang="scss">
+@import "../assets/scss/variables.scss";
+
+.VueTables__search-field {
+  margin-bottom: 45px;
+  label {
+    font-size: 18px;
+    display: block;
+    margin-bottom: 10px;
+  }
+  input {
+    font-size: 16px;
+    border: 0;
+    border-bottom: 1px solid $gray;
+    padding: 5px;
+    width: 300px;
+  }
+}
+
+.VueTables__table {
+  td {
+    padding: 15px;
+    border: 1px solid $purple-light;
+    text-align: center;
+  }
+
+  th {
+    padding: 15px;
+    span {
+      font-size: 16px;
+    }
+  }
+}
+
+thead {
+  font-weight: 700;
+}
+
+.VuePagination__count {
+  text-align: left;
+  margin: 40px 0;
+  font-size: 16px;
+  font-weight: 700;
+}
 </style>
