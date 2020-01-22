f6ad60a2deb04bc98625b933f0e1c64ae270fe45
Ana Bjelica <a.bjelica@vegaitsourcing.rs>
2020-01-22 09:37:37 +0100

    Style the reset password button

--- src/pages/ResetPasswordEmail.vue
+++ src/pages/ResetPasswordEmail.vue
@@ -32,7 +32,7 @@
         <div>
         <input
             @click.prevent="resetPassword"
-            class="btn btn-main btn__default btn__large"
+            class="btn btn-main btn__blue btn__large"
             type="submit"
             value="Resetujte lozinku"
             :disabled="!validate(form)"
